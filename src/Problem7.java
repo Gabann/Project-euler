39e5bf24f7748b14a91148c6f443754d96a2004d
Gaban <gabin.deboulogne@gmail.com>
2023-11-08 16:32:15 +0100

    Initial commit

---
+++ src/Problem7.java
@@ -0,0 +1,27 @@
+public class Problem7
+{
+	public static void main(String [] args)
+	{
+		int primeCount = 0, number = 3;
+		
+		while (primeCount < 10000)
+		{
+			int divider = 3;
+
+			while (divider < number / 2)
+			{
+				if ((number % divider == 0)){
+					number += 2;
+					divider = 1;
+				}
+
+				divider += 2;
+			}
+
+			primeCount++;
+			number += 2;
+		}
+
+		System.out.print(number - 2);
+	}
+}
