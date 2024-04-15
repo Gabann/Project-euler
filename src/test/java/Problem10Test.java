bc0a69327f64b80658fb3ad4d7f36fc2669a7a6d
Gaban <gabin.deboulogne@gmail.com>
2024-04-15 15:13:52 +0200

    Added maven and junit to project

---
+++ src/test/java/Problem10Test.java
@@ -0,0 +1,20 @@
+import org.example.Problem10;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+class Problem10Test
+{
+	@Test
+	void testIsPrime()
+	{
+		assertTrue(Problem10.isPrime(3));
+		assertTrue(Problem10.isPrime(5));
+		assertTrue(Problem10.isPrime(5839));
+		assertTrue(Problem10.isPrime(7907));
+
+		assertFalse(Problem10.isPrime(2));
+		assertFalse(Problem10.isPrime(705));
+		assertFalse(Problem10.isPrime(5000));
+	}
+}
